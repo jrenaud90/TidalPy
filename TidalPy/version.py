eca983e71a2ed62a922e7cbaf87676e7c38b0f0c
Joe Renaud - LapWIN <joe.p.renaud@gmail.com>
2019-06-20 09:36:12 -0400

    0.1.0dev15
    Major Changes
    
    Minor Changes
    
    Bugfixes
    
    Working

--- TidalPy/version.py
+++ TidalPy/version.py
@@ -4,11 +4,10 @@
 #     A = Major Release: May break backwards compatibility
 #     B = Minor Release: New features but not critical, should be backwards compatible to previous minor releases
 #     C = Bug/Hot Fixes: Bug fixes or very minor features and improvements
-# 1.2.0dev1  # Development release
-# 1.2.0a1     # Alpha Release
+# 1.2.0dev1   # Development Build
+# 1.2.0a1     # Alpha Build
 # 1.2.0b1     # Beta Release
 # 1.2.0rc1    # Release Candidate
-# 1.2.0       # Final Release
-# 1.2.0.post1 # Post Release
+# 1.2.X       # Final Release (X are bug and hot fixes that do not require the regular release cycle)
 
 version = '0.1.0dev14'
