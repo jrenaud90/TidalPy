f05bbeb88ffc5003f485083951812fddd6020257
JPR_DTPWIN <joe.p.renaud@gmail.com>
2019-07-17 16:34:29 -0400

    Fixed Setup.py and requirements.txt errors

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
             # setuptools does not use the same syntax and functionality that requirments.txt uses.
             # Repositories should be added as new 'dependency_links'
             continue
-         requirements.append(line)
+        requirements.append(line)
 
 
 setup(name='TidalPy',
@@ -25,5 +25,5 @@ setup(name='TidalPy',
       dependency_links=[
         # Make sure to include the `#egg` portion so the `install_requires` recognizes the package
         'git+ssh://git@github.com/geodynamics/burnman.git@master#egg=burnman'
-      ]
+      ],
       zip_safe=False)
