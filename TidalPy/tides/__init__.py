e8cd40fee3539e248f0fbbbd409d12a4f28c1a37
Joe Renaud - LapWIN <joe.p.renaud@gmail.com>
2019-11-11 21:17:02 -0500

    Updated Trappist Planets

--- TidalPy/tides/__init__.py
+++ TidalPy/tides/__init__.py
@@ -2,5 +2,5 @@ from .love_1d import complex_love as calc_complex_love
 from .love_1d import complex_love_general as calc_complex_love_general
 from .love_1d import effective_rigidity as calc_effective_rigidity
 from .love_1d import effective_rigidity_general as calc_effective_rigidity_general
-from .calculation import calculate_tides
+from .calculation import calculate_tides, calc_tidal_susceptibility
 from .tides import Tides
