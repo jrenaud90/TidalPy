ec18e947a73208d47afbd544bca8252c1acd4993
Jrenaud-Desk <joe.p.renaud@gmail.com>
2019-12-16 15:31:35 -0500

    Addded in static love number calculation

--- TidalPy/tides/love_1d.py
+++ TidalPy/tides/love_1d.py
@@ -23,15 +23,8 @@ def complex_love(complex_compliance: FloatArray, shear_modulus: FloatArray, eff_
         2nd order complex Love number
     """
 
-    real_j = np.real(complex_compliance)
-    imag_j = np.imag(complex_compliance)
-    real_j2 = real_j**2
-    imag_j2 = imag_j**2
-    reduced_compliance = eff_rigidity / shear_modulus
-    common_factor = (3. / 2.) * ((real_j + reduced_compliance)**2 + imag_j2)**-1
-    real_love = (real_j2 + imag_j2 + real_j * reduced_compliance) * common_factor
-    imag_love = (imag_j * reduced_compliance) * common_factor
-    cmplx_love = real_love + 1.0j * imag_love
+    rheology_factor = complex_compliance * shear_modulus
+    cmplx_love = (3. / 2.) * (1. / (1. + (eff_rigidity / rheology_factor)))
 
     return cmplx_love
 
@@ -58,17 +51,49 @@ def complex_love_general(complex_compliance: FloatArray, shear_modulus: FloatArr
         l-th order complex Love number
     """
 
-    real_j = np.real(complex_compliance)
-    imag_j = np.imag(complex_compliance)
-    real_j2 = real_j**2
-    imag_j2 = imag_j**2
-    reduced_compliance = eff_rigidity_general / shear_modulus
-    common_factor = (3. / (order_l - 1.)) * ((real_j + reduced_compliance)**2 + imag_j2)**-1
-    real_love = (real_j2 + imag_j2 + real_j * reduced_compliance) * common_factor
-    imag_love = (imag_j * reduced_compliance) * common_factor
-    cmplx_love = real_love + 1.0j * imag_love
+    rheology_factor = complex_compliance * shear_modulus
+    cmplx_love = (3. / (2. * (order_l - 1))) * (1. / (1. + (eff_rigidity_general / rheology_factor)))
 
     return cmplx_love
+
+
+@njit
+def static_love(eff_rigidity: FloatArray) -> FloatArray:
+    """ Calculate the static (non-complex) 2nd order Love number
+
+    Parameters
+    ----------
+    eff_rigidity : FloatArray
+        2nd order effective rigidity
+
+    Returns
+    -------
+    static_love : FloatArray
+        2nd order complex Love number
+    """
+
+    static_love = (3. / 2.) * (1. / (1. + eff_rigidity))
+    return static_love
+
+@njit
+def static_love_general(eff_rigidity_general: FloatArray, order_l: int = 2) -> FloatArray:
+    """ Calculate the static (non-complex) tidal Love number k.
+
+    Parameters
+    ----------
+    eff_rigidity_general : FloatArray
+        l-th order effective rigidity
+    order_l : int
+        Outermost Fourier summation integer
+
+    Returns
+    -------
+    static_love : FloatArray
+        l-th order complex Love number
+    """
+
+    static_love = (3. / (2. * (order_l - 1))) * (1. / (1. + eff_rigidity_general))
+    return static_love
 
 
 # The functions below could, in theory, be cached but with shear_modulus potentially being an array lru_cache does not
