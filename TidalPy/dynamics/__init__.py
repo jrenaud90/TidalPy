5af45e2ec9cb5841564dd6e6fe4196cb6fcefd75
Jrenaud-Desk <joe.p.renaud@gmail.com>
2020-04-27 18:30:32 -0400

    Fixed issue where pure floats would not work in dynamics funcs.

--- TidalPy/dynamics/__init__.py
+++ TidalPy/dynamics/__init__.py
@@ -1,16 +1,7 @@
 from .dual_dissipation import (eccentricity_derivative as eccentricity_derivative_dual,
-                               semi_major_axis_derivative as semi_major_axis_derivative_dual)
-from .single_dissipation import eccentricity_derivative, semi_major_axis_derivative, spin_rate_derivative
-
-diff_eqs_dual_dissipation = {
-    'eccentricity'   : eccentricity_derivative_dual,
-    'semi_major_axis': semi_major_axis_derivative_dual,
-    # Spin-rate is the same for both dual and single dissipation
-    'spin_rate'      : spin_rate_derivative
-}
-
-diff_eqs_single_dissipation = {
-    'eccentricity'   : eccentricity_derivative,
-    'semi_major_axis': semi_major_axis_derivative,
-    'spin_rate'      : spin_rate_derivative
-}
+                               eccentricity_derivative_array as eccentricity_derivative_array_dual,
+                               semi_major_axis_derivative as semi_major_axis_derivative_dual,
+                               semia_eccen_derivatives as semia_eccen_derivatives_dual,
+                               semia_eccen_derivatives_array as semia_eccen_derivatives_array_dual)
+from .single_dissipation import (eccentricity_derivative, semi_major_axis_derivative, spin_rate_derivative,
+                                 eccentricity_derivative_array, semia_eccen_derivatives_array, semia_eccen_derivatives)
--- TidalPy/dynamics/single_dissipation.py
+++ TidalPy/dynamics/single_dissipation.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 
 from ..utilities.performance.numba import njit
@@ -29,45 +31,193 @@ def spin_rate_derivative(dU_dO: FloatArray, moment_of_inertia: float, host_mass:
 
     return dspin_dt
 
-
 @njit
-def semi_major_axis_derivative(semi_major_axis: np.ndarray, orbital_motion: np.ndarray,
-                               mass_1: float, dU_dM_1: np.ndarray,
-                               mass_2: float) -> np.ndarray:
-    """ Calculate the time derivative of the semi-major axis for a single-body dissipating system
+def semi_major_axis_derivative(semi_major_axis: FloatArray, orbital_motion: FloatArray,
+                               mass_1: float, dU_dM_1: FloatArray, mass_2: float) -> FloatArray:
+    """ Calculate the time derivative of the semi-major axis for a signle-body dissipating system
+
     See Boue and Efroimsky (2019, CMDA), Eq. 116
+
     Parameters
     ----------
-    semi_major_axis : np.ndarray
+    semi_major_axis : FloatArray
         Semi-major axis in [m]
-    orbital_motion : np.ndarray
+    orbital_motion : FloatArray
         Orbital mean motion [rad s-1]
     mass_1 : float
         Mass of body 1 in [kg]
-    dU_dM_1 : np.ndarray
+    dU_dM_1 : FloatArray
         Derivative of body 1's tidal potential wrt mean anomaly
     mass_2 : float
         Mass of body 2 in [kg]
+
     Returns
     -------
-    da_dt : np.ndarray
+    da_dt : FloatArray
         Time derivative of the semi-major axis [m s-1]
     """
 
     beta_invr = (mass_1 + mass_2) / (mass_1 * mass_2)
     dR_dM_1 = -1. * beta_invr * mass_2 * dU_dM_1
+    dR_dM = dR_dM_1
 
-    da_dt = (2. / (orbital_motion * semi_major_axis)) * dR_dM_1
+    da_dt = (2. / (orbital_motion * semi_major_axis)) * dR_dM
 
     return da_dt
 
+@njit
+def eccentricity_derivative(semi_major_axis: float, orbital_motion: float, eccentricity: float,
+                            mass_1: float, dU_dM_1: float, dU_dw_1: float, mass_2: float) -> float:
+    """ Calculate the time derivative of the eccentricity for a single-body dissipating system - NonArrays Only
+
+    See Boue and Efroimsky (2019, CMDA), Eq. 117
+
+    Parameters
+    ----------
+    semi_major_axis : float
+        Semi-major axis in [m]
+    orbital_motion : float
+        Orbital mean motion [rad s-1]
+    eccentricity : float
+        Orbital eccentricity
+    mass_1 : float
+        Mass of body 1 in [kg]
+    dU_dM_1 : float
+        Derivative of body 1's tidal potential wrt mean anomaly
+    dU_dw_1 : float
+        Derivative of body 1's tidal potential wrt pericentre
+    mass_2 : float
+        Mass of body 2 in [kg]
+
+    Returns
+    -------
+    de_dt : float
+        Time derivative of the eccentricity [s-1]
+    """
+
+    e_term1 = np.sqrt(1. - eccentricity * eccentricity)
+    denom = orbital_motion * semi_major_axis * semi_major_axis * eccentricity
+
+    if abs(denom) <= float_eps:
+        # Correct for zero eccentricity
+        return 0.
+
+    beta_invr = (mass_1 + mass_2) / (mass_1 * mass_2)
+    dR_dM_1 = -1. * beta_invr * mass_2 * dU_dM_1
+    dR_dM = dR_dM_1
+
+    dR_dw_1 = -1. * beta_invr * mass_2 * dU_dw_1
+    de_dt = (e_term1 / denom) * (e_term1 * dR_dM - dR_dw_1)
+
+    return de_dt
+
 
 @njit
-def eccentricity_derivative(semi_major_axis: np.ndarray, orbital_motion: np.ndarray, eccentricity,
-                            mass_1: float, dU_dM_1: np.ndarray, dU_dw_1: np.ndarray,
-                            mass_2: float) -> np.ndarray:
-    """ Calculate the time derivative of the eccentricity for a single-body dissipating system
+def eccentricity_derivative_array(semi_major_axis: FloatArray, orbital_motion: FloatArray, eccentricity,
+                                  mass_1: float, dU_dM_1: FloatArray, dU_dw_1: FloatArray, mass_2: float) -> FloatArray:
+    """ Calculate the time derivative of the eccentricity for a single-body dissipating system - Arrays Only
+
     See Boue and Efroimsky (2019, CMDA), Eq. 117
+
+    Parameters
+    ----------
+    semi_major_axis : FloatArray
+        Semi-major axis in [m]
+    orbital_motion : FloatArray
+        Orbital mean motion [rad s-1]
+    eccentricity : FloatArray
+        Orbital eccentricity
+    mass_1 : float
+        Mass of body 1 in [kg]
+    dU_dM_1 : FloatArray
+        Derivative of body 1's tidal potential wrt mean anomaly
+    dU_dw_1 : FloatArray
+        Derivative of body 1's tidal potential wrt pericentre
+    mass_2 : float
+        Mass of body 2 in [kg]
+
+    Returns
+    -------
+    de_dt : FloatArray
+        Time derivative of the eccentricity [s-1]
+    """
+
+    e_term1 = np.sqrt(1. - eccentricity * eccentricity)
+    denom = orbital_motion * semi_major_axis * semi_major_axis * eccentricity
+
+    beta_invr = (mass_1 + mass_2) / (mass_1 * mass_2)
+    dR_dM_1 = -1. * beta_invr * mass_2 * dU_dM_1
+    dR_dM = dR_dM_1
+
+    dR_dw_1 = -1. * beta_invr * mass_2 * dU_dw_1
+    de_dt = (e_term1 / denom) * (e_term1 * dR_dM - dR_dw_1)
+
+    # Correct for zero eccentricity
+    de_dt[np.abs(denom) <= float_eps] = 0.
+
+    return de_dt
+
+
+def semia_eccen_derivatives(semi_major_axis: float, orbital_motion: float, eccentricity: float,
+                            mass_1: float, dU_dM_1: float, dU_dw_1: float, mass_2: float) \
+        -> Tuple[float, float]:
+    """ Calculate the time derivatives of semi-major axis and eccentricity for a single-body dissipating system - NonArrays Only
+
+    See Boue and Efroimsky (2019, CMDA), Eqs. 116 and 117
+
+    Parameters
+    ----------
+    semi_major_axis : float
+        Semi-major axis in [m]
+    orbital_motion : float
+        Orbital mean motion [rad s-1]
+    eccentricity : float
+        Orbital eccentricity
+    mass_1 : float
+        Mass of body 1 in [kg]
+    dU_dM_1 : float
+        Derivative of body 1's tidal potential wrt mean anomaly
+    dU_dw_1 : float
+        Derivative of body 1's tidal potential wrt pericentre
+    mass_2 : float
+        Mass of body 2 in [kg]
+
+    Returns
+    -------
+    da_dt : float
+        Time derivative of the semi-major axis [m s-1]
+    de_dt : float
+        Time derivative of the eccentricity [s-1]
+    """
+
+    # Calculate semi-major axis derivative
+    beta_invr = (mass_1 + mass_2) / (mass_1 * mass_2)
+    dR_dM_1 = -1. * beta_invr * mass_2 * dU_dM_1
+    dR_dM = dR_dM_1
+
+    da_dt = (2. / (orbital_motion * semi_major_axis)) * dR_dM
+
+    # Calculate eccentricity derivative
+    e_term1 = np.sqrt(1. - eccentricity * eccentricity)
+    denom = orbital_motion * semi_major_axis * semi_major_axis * eccentricity
+
+    if abs(denom) <= float_eps:
+        # Correct for zero eccentricity
+        de_dt = 0.
+    else:
+        dR_dw_1 = -1. * beta_invr * mass_2 * dU_dw_1
+        de_dt = (e_term1 / denom) * (e_term1 * dR_dM - dR_dw_1)
+
+    return da_dt, de_dt
+
+
+def semia_eccen_derivatives_array(semi_major_axis: np.ndarray, orbital_motion: np.ndarray, eccentricity: np.ndarray,
+                                  mass_1: float, dU_dM_1: np.ndarray, dU_dw_1: np.ndarray, mass_2: float) \
+        -> Tuple[np.ndarray, np.ndarray]:
+    """ Calculate the time derivatives of semi-major axis and eccentricity for a single-body dissipating system - Arrays Only
+
+    See Boue and Efroimsky (2019, CMDA), Eqs. 116 and 117
+
     Parameters
     ----------
     semi_major_axis : np.ndarray
@@ -84,18 +234,25 @@ def eccentricity_derivative(semi_major_axis: np.ndarray, orbital_motion: np.ndar
         Derivative of body 1's tidal potential wrt pericentre
     mass_2 : float
         Mass of body 2 in [kg]
+
     Returns
     -------
-    da_dt : np.ndarray
+    da_dt : FloatArray
+        Time derivative of the semi-major axis [m s-1]
+    de_dt : FloatArray
         Time derivative of the eccentricity [s-1]
     """
-    e_term1 = np.sqrt(1. - eccentricity * eccentricity)
-    denom = orbital_motion * semi_major_axis * semi_major_axis * eccentricity
 
-
+    # Calculate semi-major axis derivative
     beta_invr = (mass_1 + mass_2) / (mass_1 * mass_2)
     dR_dM_1 = -1. * beta_invr * mass_2 * dU_dM_1
     dR_dM = dR_dM_1
+
+    da_dt = (2. / (orbital_motion * semi_major_axis)) * dR_dM
+
+    # Calculate eccentricity derivative
+    e_term1 = np.sqrt(1. - eccentricity * eccentricity)
+    denom = orbital_motion * semi_major_axis * semi_major_axis * eccentricity
 
     dR_dw_1 = -1. * beta_invr * mass_2 * dU_dw_1
     de_dt = (e_term1 / denom) * (e_term1 * dR_dM - dR_dw_1)
@@ -103,4 +260,5 @@ def eccentricity_derivative(semi_major_axis: np.ndarray, orbital_motion: np.ndar
     # Correct for zero eccentricity
     de_dt[np.abs(denom) <= float_eps] = 0.
 
-    return de_dt
+    return da_dt, de_dt
+
