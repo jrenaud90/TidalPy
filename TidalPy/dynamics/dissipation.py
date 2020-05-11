b4e7f41f3083955e981313f5fefa58f4507defcf
Jrenaud-Desk <joe.p.renaud@gmail.com>
2020-05-11 16:52:58 -0400

    debug

--- TidalPy/dynamics/dissipation.py
+++ TidalPy/dynamics/dissipation.py
@@ -100,24 +100,26 @@ def kaula_collapse(spin_frequency, orbital_frequency, semi_major_axis,
                 dUdw_bymode.append((order_l - 2*p) * neg_imk_sgn * multiplier)
                 dUdO_bymode.append(m * neg_imk_sgn * multiplier)
 
-                if spin_coeff == -2:
+                if spin_coeff == -2 and complex_compliance_func is not fixed_q:
                     breakpoint()
+                    pass
 
 
 
-    print(complex_compliance_func)
-    from pprint import pprint
+    if complex_compliance_func is not fixed_q:
+        print(complex_compliance_func)
+        from pprint import pprint
 
-    heatings = {}
-    for sig, heating in zip(sigs, tidal_heating_bymode):
-        if sig in heatings:
-            heatings[sig] += heating
-        else:
-            heatings[sig] = heating
-    pprint(heatings)
-    pprint([(mode, tidal_heating_) for mode, tidal_heating_ in zip(sigs, dUdM_bymode)])
-    pprint([(mode, tidal_heating_) for mode, tidal_heating_ in zip(sigs, dUdw_bymode)])
-    pprint([(mode, tidal_heating_) for mode, tidal_heating_ in zip(sigs, dUdO_bymode)])
+        heatings = {}
+        for sig, heating in zip(sigs, tidal_heating_bymode):
+            if sig in heatings:
+                heatings[sig] += heating
+            else:
+                heatings[sig] = heating
+        pprint(heatings)
+        pprint([(mode, tidal_heating_) for mode, tidal_heating_ in zip(sigs, dUdM_bymode)])
+        pprint([(mode, tidal_heating_) for mode, tidal_heating_ in zip(sigs, dUdw_bymode)])
+        pprint([(mode, tidal_heating_) for mode, tidal_heating_ in zip(sigs, dUdO_bymode)])
     return tidal_modes, tidal_heating_bymode, dUdM_bymode, dUdw_bymode, dUdO_bymode
 
 
