2b9afe7a31eccf2adb2076bc11284a250952b438
Jrenaud-Desk <joe.p.renaud@gmail.com>
2021-01-15 00:01:55 -0500

    incorrect lat def

--- TidalPy/tides/potential/synchronous_low_e.py
+++ TidalPy/tides/potential/synchronous_low_e.py
@@ -6,14 +6,14 @@ import numpy as np
 
 from ...utilities.types import FloatArray
 
-def tidal_potential(radius: float, longitude: FloatArray, latitude: FloatArray,
+def tidal_potential(radius: float, longitude: FloatArray, colatitude: FloatArray,
                     orbital_frequency: FloatArray, eccentricity: FloatArray, time: FloatArray):
 
     # Associated Legendre Functions
-    p_02 = (1. / 2.) * (3. * np.cos(latitude)**2 - 1.)
-    p_22 = 3. * np.sin(latitude)**2
-    dp_02_dtheta = -3. * np.cos(latitude) * np.sin(latitude)
-    dp_22_dtheta = 6. * np.cos(latitude) * np.sin(latitude)
+    p_02 = (1. / 2.) * (3. * np.cos(colatitude)**2 - 1.)
+    p_22 = 3. * np.sin(colatitude)**2
+    dp_02_dtheta = -3. * np.cos(colatitude) * np.sin(colatitude)
+    dp_22_dtheta = 6. * np.cos(colatitude) * np.sin(colatitude)
 
     # Calculate tidal potential
     potential = radius**2 * orbital_frequency**2 * eccentricity * \
@@ -34,6 +34,6 @@ def tidal_potential(radius: float, longitude: FloatArray, latitude: FloatArray,
          (1. / 4.) * p_22 *
          (-6. * np.cos(orbital_frequency * time) * np.sin(2. * longitude) +
           8. * np.sin(orbital_frequency * time) * np.cos(2. * longitude)))
-    potential_partial_phi = potential_partial_phi / np.sin(latitude)
+    potential_partial_phi = potential_partial_phi / np.sin(colatitude)
 
     return potential, potential_partial_theta, potential_partial_phi
