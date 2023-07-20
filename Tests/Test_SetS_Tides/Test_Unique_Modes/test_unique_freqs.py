ece5b1eef0df6bb1b176ac144125bb055c9f57fe
Joe P. Renaud <joseph.p.renaud@nasa.gov>
2023-07-20 12:13:07 -0400

    new count tracker for unique freqs

--- Tests/Test_SetS_Tides/Test_Unique_Modes/test_unique_freqs.py
+++ Tests/Test_SetS_Tides/Test_Unique_Modes/test_unique_freqs.py
@@ -16,16 +16,20 @@ def test_find_unique_frequency_no_dups():
     example_dict['n-o'] = 0.5
     example_dict['2n-2o'] = 0.6
 
-    unique_frequency_list = find_unique_frequency_list(example_dict)
-    unique_frequency_dict, unique_frequency_list2 = find_unique_frequency_dict(example_dict)
+    unique_frequency_list, unique_frequency_count_list = find_unique_frequency_list(example_dict)
+    unique_frequency_dict, unique_frequency_list2, unique_frequency_count_list2 = find_unique_frequency_dict(example_dict)
 
     assert unique_frequency_list == unique_frequency_list2
+    assert unique_frequency_count_list == unique_frequency_count_list2
     assert len(unique_frequency_list) == len(example_dict)
     assert len(unique_frequency_dict) == len(example_dict)
-    for sig, freq in example_dict.items():
+    assert len(unique_frequency_count_list) == len(example_dict)
+    for i, (sig, freq) in enumerate(example_dict.items()):
         assert freq in unique_frequency_list
         assert sig in unique_frequency_dict
         assert unique_frequency_dict[sig] == freq
+        assert unique_frequency_count_list[i] == 1
+        assert unique_frequency_count_list2[i] == 1
 
 def test_find_unique_frequency_with_zeros():
     """ Test `find_unique_frequency_list` and `find_unique_frequency_dict` for a mode dict with zeros. """
@@ -36,13 +40,15 @@ def test_find_unique_frequency_with_zeros():
     example_dict['n-o'] = 0.5
     example_dict['2n-2o'] = 0.
 
-    unique_frequency_list = find_unique_frequency_list(example_dict)
-    unique_frequency_dict, unique_frequency_list2 = find_unique_frequency_dict(example_dict)
+    unique_frequency_list, unique_frequency_count_list = find_unique_frequency_list(example_dict)
+    unique_frequency_dict, unique_frequency_list2, unique_frequency_count_list2 = find_unique_frequency_dict(example_dict)
 
     assert unique_frequency_list == unique_frequency_list2
+    assert unique_frequency_count_list == unique_frequency_count_list2
     assert len(unique_frequency_list) == len(example_dict) - 2
     assert len(unique_frequency_dict) == len(example_dict) - 2
-    for sig, freq in example_dict.items():
+    assert len(unique_frequency_count_list) == len(example_dict) - 2
+    for i, (sig, freq) in enumerate(example_dict.items()):
         if freq == 0.:
             assert 0. not in unique_frequency_list
             assert sig not in unique_frequency_dict
@@ -50,6 +56,8 @@ def test_find_unique_frequency_with_zeros():
         assert freq in unique_frequency_list
         assert sig in unique_frequency_dict
         assert unique_frequency_dict[sig] == freq
+        assert unique_frequency_count_list[i] == 1
+        assert unique_frequency_count_list2[i] == 1
 
 def test_find_unique_frequency_with_exact_dups():
     """ Test `find_unique_frequency_list` and `find_unique_frequency_dict` for a mode dict with exact duplicates. """
@@ -60,22 +68,28 @@ def test_find_unique_frequency_with_exact_dups():
     example_dict['n-o'] = 0.5
     example_dict['2n-2o'] = 0.4
 
-    unique_frequency_list = find_unique_frequency_list(example_dict)
-    unique_frequency_dict, unique_frequency_list2 = find_unique_frequency_dict(example_dict)
+    unique_frequency_list, unique_frequency_count_list = find_unique_frequency_list(example_dict)
+    unique_frequency_dict, unique_frequency_list2, unique_frequency_count_list2 = find_unique_frequency_dict(example_dict)
 
     assert unique_frequency_list == unique_frequency_list2
+    assert unique_frequency_count_list == unique_frequency_count_list2
     assert len(unique_frequency_list) == len(example_dict) - 1
     assert len(unique_frequency_dict) == len(example_dict) - 1
-    for sig, freq in example_dict.items():
+    assert len(unique_frequency_count_list) == len(example_dict) - 1
+    for i, (sig, freq) in enumerate(example_dict.items()):
         if freq == 0.:
             continue
         assert freq in unique_frequency_list
         if sig in ['o', '2n-2o']:
             assert 'o 2n-2o' in unique_frequency_dict
             assert unique_frequency_dict['o 2n-2o'] == freq
+            assert unique_frequency_count_list[i] == 2
+            assert unique_frequency_count_list2[i] == 2
         else:
             assert sig in unique_frequency_dict
             assert unique_frequency_dict[sig] == freq
+            assert unique_frequency_count_list[i] == 1
+            assert unique_frequency_count_list2[i] == 1
 
 def test_find_unique_frequency_with_near_dups():
     """ Test `find_unique_frequency_list` and `find_unique_frequency_dict` for a mode dict with near duplicates. """
@@ -86,13 +100,15 @@ def test_find_unique_frequency_with_near_dups():
     example_dict['n-o'] = 0.5
     example_dict['2n-2o'] = 0.4 + 10 * MIN_FREQ
 
-    unique_frequency_list = find_unique_frequency_list(example_dict)
-    unique_frequency_dict, unique_frequency_list2 = find_unique_frequency_dict(example_dict)
+    unique_frequency_list, unique_frequency_count_list = find_unique_frequency_list(example_dict)
+    unique_frequency_dict, unique_frequency_list2, unique_frequency_count_list2 = find_unique_frequency_dict(example_dict)
 
     assert unique_frequency_list == unique_frequency_list2
+    assert unique_frequency_count_list == unique_frequency_count_list2
     assert len(unique_frequency_list) == len(example_dict)
     assert len(unique_frequency_dict) == len(example_dict)
-    for sig, freq in example_dict.items():
+    assert len(unique_frequency_count_list) == len(example_dict)
+    for i, (sig, freq) in enumerate(example_dict.items()):
         if freq == 0.:
             assert 0. not in unique_frequency_list
             assert sig not in unique_frequency_dict
@@ -100,6 +116,8 @@ def test_find_unique_frequency_with_near_dups():
         assert freq in unique_frequency_list
         assert sig in unique_frequency_dict
         assert unique_frequency_dict[sig] == freq
+        assert unique_frequency_count_list[i] == 1
+        assert unique_frequency_count_list2[i] == 1
 
 def test_find_unique_frequency_with_close_not_exact_dups():
     """ Test `find_unique_frequency_list` and `find_unique_frequency_dict` for a mode dict with close but not exact duplicates. """
@@ -110,20 +128,30 @@ def test_find_unique_frequency_with_close_not_exact_dups():
     example_dict['n-o'] = 0.5
     example_dict['2n-2o'] = 0.4 + 1 * MIN_FREQ
 
-    unique_frequency_list = find_unique_frequency_list(example_dict)
-    unique_frequency_dict, unique_frequency_list2 = find_unique_frequency_dict(example_dict)
+    unique_frequency_list, unique_frequency_count_list = find_unique_frequency_list(example_dict)
+    unique_frequency_dict, unique_frequency_list2, unique_frequency_count_list2 = find_unique_frequency_dict(example_dict)
 
     assert unique_frequency_list == unique_frequency_list2
+    assert unique_frequency_count_list == unique_frequency_count_list2
     assert len(unique_frequency_list) == len(example_dict) - 1
     assert len(unique_frequency_dict) == len(example_dict) - 1
-    for sig, freq in example_dict.items():
+    assert len(unique_frequency_count_list) == len(example_dict) - 1
+    for i, (sig, freq) in enumerate(example_dict.items()):
         if freq == 0.:
             assert 0. not in unique_frequency_list
+            assert 0. not in unique_frequency_list2
             assert sig not in unique_frequency_dict
             continue
         if sig == '2n-2o':
             assert freq not in unique_frequency_list
+            assert freq not in unique_frequency_list2
+        elif sig == 'o':
+            assert freq in unique_frequency_list
+            assert freq in unique_frequency_list2
+            assert unique_frequency_count_list[i] == 2
+            assert unique_frequency_count_list2[i] == 2
         else:
             assert freq in unique_frequency_list
+            assert freq in unique_frequency_list2
     assert 'o 2n-2o' in unique_frequency_dict
     assert unique_frequency_dict['o 2n-2o'] == example_dict['o']
