2d4958f098a1256621d305f930198a1d25b05262
Jrenaud-Desk <joe.p.renaud@gmail.com>
2023-09-25 11:18:51 -0400

    rheology tests

--- Tests/Test_SetD_Rheology_X/test_rheology_models.py
+++ Tests/Test_SetD_Rheology_X/test_rheology_models.py
@@ -63,7 +63,6 @@ def test_rheology_models_optional_args(model_name):
         # These models do not have other inputs
         assert True
     else:
-
         # Find the correct rheology class
         RheologyClass = find_rheology(model_name)
 
@@ -101,58 +100,56 @@ def test_rheology_models_optional_args(model_name):
             assert math.isclose(complex_shear.imag, 2840191640.197709)
 
 
-    @pytest.mark.parametrize('model_name', expected_results.keys())
-    def test_rheology_models_changing_args(model_name):
-        if model_name in ('off', 'elastic', 'viscous', 'newton', 'maxwell'):
-            # These models do not have other inputs
-            assert True
-        else:
-
-
-            # Find the correct rheology class
-            RheologyClass = find_rheology(model_name)
-
-            # Instantiate with no args
-            rheology_instance = RheologyClass()
-
-            # Run once
-            complex_shear = rheology_instance(frequency, shear_mod, viscosity)
-
-            # Change args
-            if model_name in ('sundberg', 'sundbergcooper'):
-                # S-C has 4 args.
-                rheology_instance.change_args((0.9, 0.9, 0.9, 0.9))
-            else:
-                # Others have 2.
-                rheology_instance.change_args((0.9, 0.9))
-
-            # Run and make sure results are different.
-            complex_shear_2 = rheology_instance(frequency, shear_mod, viscosity)
-            assert not math.isclose(complex_shear.real, complex_shear_2.real)
-            assert not math.isclose(complex_shear.imag, complex_shear_2.imag)
-
-
-    @pytest.mark.parametrize('model_name', expected_results.keys())
-    @pytest.mark.parametrize('vectorized_freq', (True, False))
-    def test_rheology_models_arrays(model_name, vectorized_freq):
+@pytest.mark.parametrize('model_name', expected_results.keys())
+def test_rheology_models_changing_args(model_name):
+    if model_name in ('off', 'elastic', 'viscous', 'newton', 'maxwell'):
+        # These models do not have other inputs
+        assert True
+    else:
         # Find the correct rheology class
         RheologyClass = find_rheology(model_name)
 
         # Instantiate with no args
         rheology_instance = RheologyClass()
 
-        N = 100
-        output_array = np.empty(N, dtype=np.complex128, order='C')
-        if vectorized_freq:
-            # With a vectorized frequency
-            freq_array = frequency * np.ones(N, dtype=np.float64, order='C')
-            rheology_instance.vectorize_frequency(freq_array, shear_mod, viscosity, output_array)
-        else:
-            # With vectorized thermals (shear and viscosity)
-            shear_array = shear_mod * np.ones(N, dtype=np.float64, order='C')
-            visc_array = viscosity * np.ones(N, dtype=np.float64, order='C')
-            rheology_instance.vectorize_modulus_viscosity(frequency, shear_array, visc_array, output_array)
+        # Run once
+        complex_shear = rheology_instance(frequency, shear_mod, viscosity)
 
-        # Make an expected array and test.
-        expected_array = expected_results[model_name][1] * np.ones(N, dtype=np.complex128, order='C')
-        assert np.allclose(output_array, expected_array)
+        # Change args
+        if model_name in ('sundberg', 'sundbergcooper'):
+            # S-C has 4 args.
+            rheology_instance.change_args((0.9, 0.9, 0.9, 0.9))
+        else:
+            # Others have 2.
+            rheology_instance.change_args((0.9, 0.9))
+
+        # Run and make sure results are different.
+        complex_shear_2 = rheology_instance(frequency, shear_mod, viscosity)
+        assert not math.isclose(complex_shear.real, complex_shear_2.real)
+        assert not math.isclose(complex_shear.imag, complex_shear_2.imag)
+
+
+@pytest.mark.parametrize('model_name', expected_results.keys())
+@pytest.mark.parametrize('vectorized_freq', (True, False))
+def test_rheology_models_arrays(model_name, vectorized_freq):
+    # Find the correct rheology class
+    RheologyClass = find_rheology(model_name)
+
+    # Instantiate with no args
+    rheology_instance = RheologyClass()
+
+    N = 100
+    output_array = np.empty(N, dtype=np.complex128, order='C')
+    if vectorized_freq:
+        # With a vectorized frequency
+        freq_array = frequency * np.ones(N, dtype=np.float64, order='C')
+        rheology_instance.vectorize_frequency(freq_array, shear_mod, viscosity, output_array)
+    else:
+        # With vectorized thermals (shear and viscosity)
+        shear_array = shear_mod * np.ones(N, dtype=np.float64, order='C')
+        visc_array = viscosity * np.ones(N, dtype=np.float64, order='C')
+        rheology_instance.vectorize_modulus_viscosity(frequency, shear_array, visc_array, output_array)
+
+    # Make an expected array and test.
+    expected_array = expected_results[model_name][1] * np.ones(N, dtype=np.complex128, order='C')
+    assert np.allclose(output_array, expected_array)
