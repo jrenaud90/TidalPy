7d1140cc8f0721e50729a1121a74162e626ce0ce
Joe P. Renaud <joe.p.renaud@gmail.com>
2024-08-27 13:59:17 -0400

    Added interp cpp funcs

---
+++ TidalPy/Material/material_.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+class Material
+{
+private:
+    /* data */
+public:
+    Material() { };
+    virtual ~Material();
+    Material(void* eos_input);
+};
+
+Material::Material(/* args */)
+{
+}
+
+
+
+Material::~Material()
+{
+}
+
