1e49a84a5548bf22e00c774922c9e7422c2a48cd
Jrenaud-Desk <joe.p.renaud@gmail.com>
2024-12-31 11:15:06 -0500

    Converted all C files to C++

--- TidalPy/utilities/classes/base_x.pyx
+++ TidalPy/utilities/classes/base_x.pyx
@@ -1,4 +1,4 @@
-# distutils: language = c
+# distutils: language = c++
 # cython: boundscheck=False, wraparound=False, nonecheck=False, cdivision=True, initializedcheck=False
 
 from TidalPy import config
