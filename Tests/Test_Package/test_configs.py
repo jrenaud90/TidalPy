b4fe174046352286305cd33142ebbda72b45b04a
Jrenaud-Desk <joe.p.renaud@gmail.com>
2025-01-04 14:12:57 -0500

    Added config override test for dicts

--- Tests/Test_Package/test_configs.py
+++ Tests/Test_Package/test_configs.py
@@ -1,10 +1,12 @@
 import pathlib
 
-import TidalPy
 
-
-def test_override_config():
+def test_override_config_from_file():
     """ Tests that we can override TidalPy's configurations by providing a new config file. """
+    import TidalPy
+
+    # Reset to default in case it was already overridden this session
+    TidalPy.reinit('default')
 
     original_file_level    = TidalPy.config['logging']['file_level']
     original_console_level = TidalPy.config['logging']['console_level']
@@ -28,3 +30,31 @@ def test_override_config():
     # Delete the temp file
     fp = pathlib.Path(config_path)
     fp.unlink()
+
+
+def test_override_config_from_dict():
+    """ Tests that we can override TidalPy's configurations by providing a new config dict. """
+    import TidalPy
+
+    # Reset to default in case it was already overridden this session
+    TidalPy.reinit('default')
+
+    original_file_level    = TidalPy.config['logging']['file_level']
+    original_console_level = TidalPy.config['logging']['console_level']
+
+    # Make a new config file that changes one of the above.
+    new_config = dict(
+        logging = dict(
+            file_level = "INFO"
+        )
+    )
+    
+    # Tell TidalPy to override the configs
+    TidalPy.reinit(new_config)
+
+    # Check that the config was updated
+    assert TidalPy.config['logging']['file_level'] != original_file_level
+    assert TidalPy.config['logging']['file_level'] == "INFO"
+
+    # Check that the other config was unaffected.
+    assert TidalPy.config['logging']['console_level'] == original_console_level
